b71d819ec30ca12a9b31af33993cef5c29d1b26c
Podarkes <yevgen.gryaznov@gmail.com>
2024-10-03 01:03:51 +0200

    Implement masking

--- main.js
+++ main.js
@@ -1,30 +1,71 @@
 import { state } from './state.js'
 import { handleKeyDown } from './keylisteners.js'
 
-const { Application, Graphics } = PIXI; 
+const { Application, Assets, BitmapText, Container, Graphics, Sprite, Texture } = PIXI; 
 
 const app = new Application();
 globalThis.__PIXI_APP__ = app;
 
-await app.init({ backgroundAlpha: 0, width: 600, height: 600 })
+await app.init({ backgroundAlpha: 0, width: 1000, height: 750 })
      
     document.getElementById('canvas').appendChild(app.canvas);
     
     const ctx = new Graphics();
-    app.stage.addChild(ctx);
+let mask = new Graphics()
+    .circle(state.planet.x, state.planet.y, state.planet.radius) .fill(0xffffff);
 
-    let color = 'blue';
+let maskContainer = new Container();
 
-    function drawCircle(color) {
+maskContainer.mask = mask;
+
+
+maskContainer.addChild(mask);
+maskContainer.position.set(0,0);
+
+app.stage.addChild(maskContainer);
+maskContainer.addChild(ctx);
+    const s1 = Sprite.from(Texture.WHITE);
+    s1.width = 100;
+    s1.height = 100;
+    s1.tint = 0x00FF00;
+    s1.x = 300;
+    s1.y = 300;
+   // app.stage.addChild(s1);
+
+
+    let debugText = 'Loading...';
+    // Load bitmap font
+    await Assets.load('https://pixijs.com/assets/bitmap-font/desyrel.xml');
+
+    const bitmapFontText = new BitmapText({
+        text: debugText,
+        style: {
+            fontFamily: 'sans',
+            fontSize: 20,
+            align: 'left',
+        },
+    });
+
+    bitmapFontText.x = 50;
+    bitmapFontText.y = 200;
+    
+    app.stage.addChild(bitmapFontText);
+    
+    function drawRect(x, y, w, h, color) {
+        ctx.rect(x, y, w, h)
+            .fill(color);
+    }
+
+    function drawCircle() {
         ctx.clear();
         ctx
-        .circle(600 / 2, 600 / 2, 200)
-        .fill(color);
+        .circle(state.planet.x, state.planet.y, state.planet.radius)
+        .fill('blue');
     }
     
     let { x , y } = state.dino;
 
-    drawCircle(color);
+    drawCircle();
     drawDino(x, y);
 
     let elapsed = 0.0;
@@ -34,8 +75,19 @@ await app.init({ backgroundAlpha: 0, width: 600, height: 600 })
 //    const r = Math.sin(elapsed) * 127 + 128;
 //    const b = Math.cos(elapsed) * 127 + 128;
 //    color = (r << 16) | (b);
-//        drawCircle(color);
+//  drawCircle(color);    
+      drawCircle(600 / 2, 600 / 2, 200);
+    for (let i = 0; i < 3; i++) {
+        let con = state.continents[i];
+        if (con.x < 0) {
+            con.x = 800;
+        }
+        
+        drawRect(con.x--, con.y, con.width, con.height, con.color); 
+        // ctx.fillRect(con.x, con.y, con.width, con.height);
+    }
         drawDino(state.dino.x, state.dino.y);
+        drawDebugInfo();
     });
     
 
@@ -58,8 +110,7 @@ let font = "18px monospace";
 
 
 function drawDebugInfo() {
-    ctx.fillStyle = "white";
-    ctx.font = font; 
+    
     displayX = Math.floor(state.planet.x);
     if (Math.floor(radius) % 50 == 0) {
         displayX = Math.floor(state.planet.x);
@@ -68,45 +119,13 @@ function drawDebugInfo() {
     if (Math.floor(minRadius) % 5 == 0) {
         displayMinRadius = Math.floor(minRadius);
     }
-    var text1 = `dino:   x = ${Math.floor(state.dino.x)}, y = ${Math.floor(state.dino.y)}, d = ${Math.floor(state.dino.distance)}, s = ${state.dino.step}`;
-    var text2 = `planet: x = ${displayX}, y = ${displayY}, r = ${displayMinRadius}`;
-    ctx.fillText(text1, 25, 50);
-    ctx.fillText(text2, 25, 75);
+    debugText = `dino:   x = ${Math.floor(state.dino.x)}, y = ${Math.floor(state.dino.y)}, d = ${Math.floor(state.dino.distance)}, s = ${state.dino.step}`;
+    debugText = debugText + '\n' + `planet: x = ${displayX}, y = ${displayY}, r = ${displayMinRadius}`;
+    bitmapFontText.text = debugText;
 }
 
-function drawCircleLegacy() {
-	ctx.beginPath();
-
-	ctx.fillStyle = 'blue';
-
-	minRadius = state.planet.radius;
-    ctx.arc(state.planet.x, state.planet.y, state.planet.radius, 0, 2*Math.PI);
-    ctx.closePath();
-	ctx.stroke();
-    ctx.fill();
-}
 
 function drawDino(x, y) {
-    // ctx.clear();
     ctx.circle(x, y, 10).fill('green');
 }
-
-function draw() {
-
-    ctx.clearRect(0, 0, 1000, 750);
-
-    drawDebugInfo();
-	drawCircle();
-
-    for (let i = 0; i < 3; i++) {
-        let con = state.continents[i];
-        ctx.fillStyle = con.color;
-        ctx.fillRect(con.x, con.y, con.width, con.height);
-    }
-
-    drawDino(state.dino.x, state.dino.y);
-     
-    requestAnimationFrame(draw);
-}
-
 
